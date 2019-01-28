9d3854daa0ea01b5be565d6f692e152ebb81007e
wangzhai <1215136016@qq.com>
2019-01-28 23:08:31 +0800

    webpack

--- Vue-Study/day08/BStudy/webpack-demo/src/main.js
+++ Vue-Study/day08/BStudy/webpack-demo/src/main.js
@@ -2,5 +2,5 @@ import $ from 'jquery'
 
 $(function () {
     $('li:odd').css('backgroundColor', 'orange')
-    $('li:even').css('backgroundColor','red')
+    $('li:even').css('backgroundColor', 'red')
 })
